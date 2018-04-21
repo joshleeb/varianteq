50a9fd168219527f2712795c545b064d5ccac9a4
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2018-04-21 20:12:53 +1000

    Error when #[derive(VariantEq)] on non-enum types

--- src/varianteq.rs
+++ src/varianteq.rs
@@ -1,9 +1,19 @@
-use proc_macro::Diagnostic;
+use proc_macro::{Diagnostic, Level};
 use quote::Tokens;
-use syn::DeriveInput;
+use syn::{Data, DeriveInput};
 
 pub fn derive(item: DeriveInput) -> Result<Tokens, Diagnostic> {
     let name = item.ident;
+    let _data = match item.data {
+        Data::Enum(d) => d,
+        _ => {
+            return Err(Diagnostic::new(
+                Level::Error,
+                "#[derive(VariantEq)] is only defined for enums",
+            ))
+        }
+    };
+
     Ok(quote! {
         impl PartialEq for #name {
             fn eq(&self, other: &#name) -> bool {
