4ae0d12c5ba27f33ce35eae166ecbbb3d59b5432
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2018-04-21 20:02:02 +1000

    Include Diagnostic to handle errors and warnings

--- src/lib.rs
+++ src/lib.rs
@@ -7,11 +7,28 @@ extern crate proc_macro;
 extern crate proc_macro2;
 extern crate syn;
 
-use proc_macro::TokenStream;
+use proc_macro::{Diagnostic, TokenStream};
+use quote::Tokens;
+use syn::{parse2, DeriveInput};
 
 mod varianteq;
 
+type DeriveFn = fn(DeriveInput) -> Result<Tokens, Diagnostic>;
+
 #[proc_macro_derive(VariantEq)]
 pub fn varianteq_derive(tokens: TokenStream) -> TokenStream {
-    varianteq::derive(tokens.into()).into()
+    expand_derive(tokens, varianteq::derive)
+}
+
+fn expand_derive(tokens: TokenStream, derive: DeriveFn) -> TokenStream {
+    let item = parse2(tokens.into()).unwrap();
+    match derive(item) {
+        Ok(tokens) => tokens.into(),
+        Err(err) => handle_derive_err(err),
+    }
+}
+
+fn handle_derive_err(err: Diagnostic) -> TokenStream {
+    err.emit();
+    "".parse().unwrap()
 }
--- src/varianteq.rs
+++ src/varianteq.rs
@@ -1,17 +1,15 @@
-use proc_macro2::TokenStream;
+use proc_macro::Diagnostic;
 use quote::Tokens;
-use syn::{parse2, DeriveInput};
+use syn::DeriveInput;
 
-pub fn derive(tokens: TokenStream) -> Tokens {
-    let ast: DeriveInput = parse2(tokens).unwrap();
-    let name = ast.ident;
-
-    quote! {
+pub fn derive(item: DeriveInput) -> Result<Tokens, Diagnostic> {
+    let name = item.ident;
+    Ok(quote! {
         impl PartialEq for #name {
             fn eq(&self, other: &#name) -> bool {
                 true    // TODO(joshleeb): Implement.
             }
         }
         impl Eq for #name {}
-    }
+    })
 }
