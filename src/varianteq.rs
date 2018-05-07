38eef4461e575f84c0e606a8b334da93cff2226d
Josh Leeb-du Toit <josh.leebdutoit@gmail.com>
2018-05-07 16:37:48 +1000

    Add item generics to PartialEq and Eq impl

--- src/varianteq.rs
+++ src/varianteq.rs
@@ -6,13 +6,15 @@ pub fn derive(item: DeriveInput) -> Result<Tokens, Diagnostic> {
     check_enum_data(item.data)?;
 
     let ident = item.ident;
+    let (impl_generics, ty_generics, where_clause) = item.generics.split_for_impl();
+
     Ok(quote! {
-        impl PartialEq for #ident {
+        impl #impl_generics PartialEq for #ident #ty_generics #where_clause {
             fn eq(&self, other: &#ident) -> bool {
                 ::std::mem::discriminant(self) == ::std::mem::discriminant(other)
             }
         }
-        impl Eq for #ident {}
+        impl #impl_generics Eq for #ident #ty_generics #where_clause {}
     })
 }
 
